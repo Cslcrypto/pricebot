f75b3796f24ee8d2ba097756c06b6359d575a511
Cslcrypto <111734416+Cslcrypto@users.noreply.github.com>
2022-08-28 12:35:19 +0530

    Updated Price-Tracker Main File.

--- pcsPriceTracker.py
+++ pcsPriceTracker.py
@@ -11,24 +11,26 @@ pairAddress = web3.toChecksumAddress('0x955b745363Dbca94F8F61C447da731e9Be4BAab8
 wbnbContract = web3.toChecksumAddress('0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c')
 wbnbABI = '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]'
 contract = web3.eth.contract(address=wbnbContract, abi=wbnbABI)
+contract2 = web3.eth.contract(address=tokenAddress, abi=wbnbABI)
 
-botToken = '5526163505:AAEfG2W58BpKWBRo1OPAXunH4vuCICkrb6s'
+botToken = '5526163505:AAGVUiHoLajqO5yND4zjNq638QqoQI-LrHo'
 bot = Bot(botToken)
 updater=Updater(botToken,use_context=True)
 dispatcher=updater.dispatcher
 
 def test_function(update:Update,context:CallbackContext):
-    tokenPrice = requests.get(f'https://api.pancakeswap.info/api/v2/tokens/{tokenAddress}').json()
+    # tokenPrice = requests.get(f'https://api.pancakeswap.info/api/v2/tokens/{tokenAddress}').json()
     BnbData = requests.get('https://api.binance.com/api/v3/ticker/price?symbol=BNBUSDT').json()
     BnbPrice = float(BnbData['price'])
-    liquidity = contract.functions.balanceOf(pairAddress).call()
+    BnbLiquidity = contract.functions.balanceOf(pairAddress).call()
+    CslLiquidity = contract2.functions.balanceOf(pairAddress).call()
     
     bot.send_message(chat_id=update.effective_chat.id, text=f'''
 Name : Colossal Finance
 Symbol : CSL
-CSL Price : ${round(float(tokenPrice['data']['price']), 7)}
+CSL Price : ${round((float(web3.fromWei(BnbLiquidity, 'ether'))/float(web3.fromWei(CslLiquidity, 'ether')))*BnbPrice, 7)}
 BNB Price : ${BnbPrice}
-LP Holdings : ${int(float(web3.fromWei(liquidity, 'ether'))*(BnbPrice*2))}
+LP Holdings : ${int(float(web3.fromWei(BnbLiquidity, 'ether'))*(BnbPrice*2))}
 ''')
 
 start_value=CommandHandler('price', test_function)
